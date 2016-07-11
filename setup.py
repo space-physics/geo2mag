fc097e7970c1087dfbbf4bcb92c7a0cf05fdda8e
scivision <scivision@users.noreply.github.com>
2016-07-11 18:10:44 -0400

    cleanup travis

--- setup.py
+++ setup.py
@@ -3,17 +3,15 @@ from setuptools import setup
 import subprocess
 
 try:
-    subprocess.call(['conda','install','--yes','--file','requirements.txt'])
+    subprocess.call(['conda','install','--file','requirements.txt'])
 except Exception as e:
     pass
 
 
 setup(name='geo2mag',
 	  description='Convert geodetic to geomagnetic coordinates',
-	  author='Michael Hirsch',
+      author='Michael Hirsch',
 	  url='https://github.com/scienceopen/geo2mag',
       install_requires=['fastkml','pathlib2'],
-      dependency_links = [],
-      extras_require={},
       packages=['geo2mag'],
 	  )

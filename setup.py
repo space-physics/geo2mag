15cce9a72bf2c207e4dab8e42e776d237d4fa059
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-11-20 02:34:43 -0500

    template

--- setup.py
+++ setup.py
@@ -1,21 +1,11 @@
 #!/usr/bin/env python
-req = ['nose','numpy','lxml','beautifulsoup4','html5lib', 'requests', 'pandas', 'python-dateutil']
-pipreq=['fastkml']
-
-import pip
-try:
-    import conda.cli
-    conda.cli.main('install',*req)
-except Exception as e:
-    pip.main(['install'] + req)
-pip.main(['install'] + pipreq)
-
+req = ['nose','numpy','lxml','beautifulsoup4','html5lib', 'requests', 'pandas', 'python-dateutil','fastkml']
 # %%
-from setuptools import setup
+from setuptools import setup,find_packages
 
 setup(name='geo2mag',
-      packages=['geo2mag'],
-      install_requires=req+pipreq,
+      packages=find_packages(),
+      install_requires=req,
       author="Michael Hirsch, Ph.D.",
       url="https://github.com/scivision/geo2mag",
 	  )

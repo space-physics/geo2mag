cdfb146bb61aa375c28eba1fe02fb40a1cd1df45
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-05-12 10:45:12 -0400

    move tests/ under pkg

--- igrfcoord/tests/test_basic.py
+++ igrfcoord/tests/test_basic.py
@@ -11,12 +11,12 @@ R = Path(__file__).parent
 def test_geo2mag():
     latlon = ic.convert((65, -148, 2015), "geomag")
 
-    assert latlon["mlat"].item() == approx(65.56)
-    assert latlon["mlon"].item() == approx(-96.46)
+    assert latlon["mlat"].item() == approx(65.55)
+    assert latlon["mlon"].item() == approx(-96.47)
 
 
 def test_mag2eo():
-    latlon = ic.convert((65.56, -96.46, 2015), "geodetic")
+    latlon = ic.convert((65.55, -96.47, 2015), "geodetic")
 
     assert latlon["glat"].item() == approx(65, abs=0.01)
     assert latlon["glon"].item() == approx(-148, abs=0.01)
@@ -30,8 +30,8 @@ def test_kml():
 
     assert latlon["glat"].item() == approx(65, abs=0.01)
     assert latlon["glon"].item() == approx(-148, abs=0.01)
-    assert latlon["mlat"].item() == approx(65.56)
-    assert latlon["mlon"].item() == approx(-96.46)
+    assert latlon["mlat"].item() == approx(65.55)
+    assert latlon["mlon"].item() == approx(-96.47)
 
 
 if __name__ == "__main__":
